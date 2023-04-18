020ce902ebaa6f298b7ac6556b188654171f1a9b
bri25yu <bri25yu@gmail.com>
2023-04-17 22:44:33 -0700

    returning to some defaults for best experiments

--- march/experiments/baseline.py
+++ march/experiments/baseline.py
@@ -24,7 +24,7 @@ class BestExperiment(BaselineExperiment):
     def get_training_arguments(self) -> Seq2SeqTrainingArguments:
         default_training_arguments = self.load_default_training_arguments()
 
-        target_total_batch_size = 64 * 16 * 4  # 64 * 16 is the baseline. This is 4B tokens seen
+        target_total_batch_size = 64 * 16
         train_batch_size = 4
         assert target_total_batch_size % train_batch_size == 0
         gradient_accumulation_steps = target_total_batch_size // train_batch_size
@@ -33,9 +33,7 @@ class BestExperiment(BaselineExperiment):
         default_training_arguments["per_device_eval_batch_size"] = train_batch_size * 2
         default_training_arguments["gradient_accumulation_steps"] = gradient_accumulation_steps
 
-        default_training_arguments["lr_scheduler_type"] = "constant_with_warmup"
-
-        return Seq2SeqTrainingArguments(self.output_dir, warmup_steps=2000, **default_training_arguments)
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
 
     def get_model(self) -> TransformerBase:
         config = TransformerConfig(dim_model=1024, num_layers=48)  # Match t5-large
