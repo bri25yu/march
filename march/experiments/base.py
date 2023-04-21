2e1e1c9b0346ded9d52e613b400703e12ec379f4
bri25yu <bri25yu@gmail.com>
2023-04-20 20:10:17 -0700

    no longer using torch compile under deepspeed

--- march/experiments/base.py
+++ march/experiments/base.py
@@ -63,7 +63,7 @@ class ExperimentBase(ABC):
             total_batch_size = batch_size_per_gpu * args_dict["gradient_accumulation_steps"]
             args_dict["gradient_accumulation_steps"] = total_batch_size // (batch_size_per_gpu * num_gpus)
 
-            args_dict["bf16"] = True
+            args_dict["torch_compile"] = False
 
         return args_dict
 
