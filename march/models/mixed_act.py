2735044f6d43055d5daab64d5f7fc44a230f4174
hlillemark <hlillemark@gmail.com>
2023-04-24 10:19:29 -0700

    Fix other init weights

--- march/models/mixed_act.py
+++ march/models/mixed_act.py
@@ -31,6 +31,8 @@ class GatedLinearUnitFeedforward(TransformerComponentBase):
         self.gate_projection = Linear(config.dim_model, config.dim_feedforward, bias=False)
         self.down_projection = Linear(config.dim_feedforward, config.dim_model, bias=False)
 
+        self.init_weights()
+
     def init_weights(self) -> None:
         config = self.config
 
@@ -75,6 +77,8 @@ class MixedActFeedforward(TransformerComponentBase):
         self.up_projection_fast = Linear(config.dim_model, config.dim_feedforward, bias=False)
         self.down_projection = Linear(config.dim_feedforward, config.dim_model, bias=False)
 
+        self.init_weights()
+
     def init_weights(self) -> None:
         config = self.config
 
@@ -117,6 +121,8 @@ class MixedActSumOverMeanFeedforward(TransformerComponentBase):
         self.up_projection_slow = Linear(config.dim_model, config.dim_feedforward, bias=False)
         self.up_projection_fast = Linear(config.dim_model, config.dim_feedforward, bias=False)
         self.down_projection = Linear(config.dim_feedforward, config.dim_model, bias=False)
+
+        self.init_weights()
 
     def init_weights(self) -> None:
         config = self.config
