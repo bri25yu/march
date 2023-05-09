2129526046596a6ffa7a50403a775ed6cb6e81cf
hlillemark <hlillemark@gmail.com>
2023-05-09 10:14:39 -0700

    Push new tp embeddings exps

--- march/experiments/tests.py
+++ march/experiments/tests.py
@@ -268,3 +268,21 @@ class TPEmbeddingsBaseExperiment(BaselineExperiment):
     def get_model(self) -> TransformerBase:
         config = TPEmbeddingsConfig(num_layers=20,num_roles=89)
         return TPEmbeddingsTransformer(config)
+
+
+class TPEmbeddingsBase2Experiment(BaselineExperiment):
+    def get_model(self) -> TransformerBase:
+        config = TPEmbeddingsConfig(num_layers=16,num_roles=222)
+        return TPEmbeddingsTransformer(config)
+
+
+class TPEmbeddingsBase3Experiment(BaselineExperiment):
+    def get_model(self) -> TransformerBase:
+        config = TPEmbeddingsConfig(num_layers=12,num_roles=445)
+        return TPEmbeddingsTransformer(config)
+
+
+class TPEmbeddingsBase4Experiment(BaselineExperiment):
+    def get_model(self) -> TransformerBase:
+        config = TPEmbeddingsConfig(num_layers=6,num_roles=1335)
+        return TPEmbeddingsTransformer(config)
