c850d76702e8caa2ffaa6cf20ef84fdf11cc1f2b
hlillemark <hlillemark@gmail.com>
2023-05-10 00:43:36 -0700

    Make config for larger gpu and add medium exp

--- march/experiments/tp_embeddings_before_w_o.py
+++ march/experiments/tp_embeddings_before_w_o.py
@@ -10,11 +10,18 @@ class TPEmbeddingsBeforeW_oEncDecSharedExperiment(BaselineExperiment):
 
 
 class TPEmbeddingsBeforeW_oEncDecShared2Experiment(BaselineExperiment):
-    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
-        default_training_arguments = self.load_default_training_arguments()
-        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+    def get_model(self) -> TransformerBase:
+        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=5000)
+        return TPEmbeddingsBeforeW_oEncDecSharedTransformer(config)
 
-        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+
+class TPEmbeddingsBeforeW_oEncDecShared3Experiment(BaselineExperiment):
+    # Necessary on smaller gpus
+    # def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+    #     default_training_arguments = self.load_default_training_arguments()
+    #     default_training_arguments = update_with_half_batch_size(default_training_arguments)
+
+    #     return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
 
     def get_model(self) -> TransformerBase:
         config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000)
@@ -22,26 +29,28 @@ class TPEmbeddingsBeforeW_oEncDecShared2Experiment(BaselineExperiment):
 
 
 class TPEmbeddingsBeforeW_oEncDecSharedCross2Experiment(BaselineExperiment):
-    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
-        default_training_arguments = self.load_default_training_arguments()
-        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+    # May be necessary on smaller gpus
+    # def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+    #     default_training_arguments = self.load_default_training_arguments()
+    #     default_training_arguments = update_with_half_batch_size(default_training_arguments)
 
-        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+    #     return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
         
     def get_model(self) -> TransformerBase:
-        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000,role_attention_type=1)
+        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=5000,role_attention_type=1)
         return TPEmbeddingsBeforeW_oEncDecSharedTransformer(config)
 
 
 class TPEmbeddingsBeforeW_oEncDecSharedBoth2Experiment(BaselineExperiment):
-    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
-        default_training_arguments = self.load_default_training_arguments()
-        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+    # May be necessary on smaller gpus
+    # def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+    #     default_training_arguments = self.load_default_training_arguments()
+    #     default_training_arguments = update_with_half_batch_size(default_training_arguments)
 
-        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+    #     return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
         
     def get_model(self) -> TransformerBase:
-        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000,role_attention_type=2)
+        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=5000,role_attention_type=2)
         return TPEmbeddingsBeforeW_oEncDecSharedTransformer(config)
 
 
@@ -52,12 +61,13 @@ class TPEmbeddingsBeforeW_oEncDecNotSharedExperiment(BaselineExperiment):
 
 
 class TPEmbeddingsBeforeW_oEncDecNotShared2Experiment(BaselineExperiment):
-    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
-        default_training_arguments = self.load_default_training_arguments()
-        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+    # May be necessary on smaller gpus
+    # def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+    #     default_training_arguments = self.load_default_training_arguments()
+    #     default_training_arguments = update_with_half_batch_size(default_training_arguments)
 
-        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+    #     return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
         
     def get_model(self) -> TransformerBase:
-        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000)
+        config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=5000)
         return TPEmbeddingsBeforeW_oEncDecNotSharedTransformer(config)
