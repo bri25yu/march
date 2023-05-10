c6a4a2094fe703970f0127c4c9274491ab241099
hlillemark <hlillemark@gmail.com>
2023-05-10 00:19:44 -0700

    Update tp embeddings exps with half batch size

--- march/experiments/tp_embeddings_before_w_o.py
+++ march/experiments/tp_embeddings_before_w_o.py
@@ -10,18 +10,36 @@ class TPEmbeddingsBeforeW_oEncDecSharedExperiment(BaselineExperiment):
 
 
 class TPEmbeddingsBeforeW_oEncDecShared2Experiment(BaselineExperiment):
+    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+        default_training_arguments = self.load_default_training_arguments()
+        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+
     def get_model(self) -> TransformerBase:
         config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000)
         return TPEmbeddingsBeforeW_oEncDecSharedTransformer(config)
 
 
 class TPEmbeddingsBeforeW_oEncDecSharedCross2Experiment(BaselineExperiment):
+    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+        default_training_arguments = self.load_default_training_arguments()
+        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+        
     def get_model(self) -> TransformerBase:
         config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000,role_attention_type=1)
         return TPEmbeddingsBeforeW_oEncDecSharedTransformer(config)
 
 
 class TPEmbeddingsBeforeW_oEncDecSharedBoth2Experiment(BaselineExperiment):
+    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+        default_training_arguments = self.load_default_training_arguments()
+        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+        
     def get_model(self) -> TransformerBase:
         config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000,role_attention_type=2)
         return TPEmbeddingsBeforeW_oEncDecSharedTransformer(config)
@@ -34,6 +52,12 @@ class TPEmbeddingsBeforeW_oEncDecNotSharedExperiment(BaselineExperiment):
 
 
 class TPEmbeddingsBeforeW_oEncDecNotShared2Experiment(BaselineExperiment):
+    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+        default_training_arguments = self.load_default_training_arguments()
+        default_training_arguments = update_with_half_batch_size(default_training_arguments)
+
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+        
     def get_model(self) -> TransformerBase:
         config = TPEmbeddingsBeforeW_oConfig(num_layers=24,num_roles=10000)
         return TPEmbeddingsBeforeW_oEncDecNotSharedTransformer(config)
