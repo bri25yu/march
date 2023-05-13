23bdf8a2dc8a848dc2e1da7b7a21792f45aa40ff
bri25yu <bri25yu@gmail.com>
2023-05-13 12:59:51 -0700

    copying over t5 relative positino bias

--- march/models/baseline.py
+++ march/models/baseline.py
@@ -243,8 +243,6 @@ class BaselineAttention(AttentionBase):
         if self.has_relative_attention_bias:
             self.relative_attention_bias.weight.data.normal_(mean=0.0, std=config.dim_model ** -0.5)
 
-    # TODO implement relative position bias
-
     def forward(
         self,
         input_embeds: SequenceInputEmbeds,
@@ -268,16 +266,23 @@ class BaselineAttention(AttentionBase):
 
         attention_logits: MultiHeadedAttention = matmul(query, key.transpose(2, 3))
 
-        if attention_mask is not None:
-            if len(attention_mask.size()) == 2:
-                query_length = 1
-                batch_size, key_length = attention_mask.size()
-            elif len(attention_mask.size()) == 3:
-                batch_size, query_length, key_length = attention_mask.size()
+        if attention_mask is None:
+            batch_size, query_length, _ = input_embeds.size()
+            key_length = query_length
+        elif len(attention_mask.size()) == 2:
+            query_length = 1
+            batch_size, key_length = attention_mask.size()
+        elif len(attention_mask.size()) == 3:
+            batch_size, query_length, key_length = attention_mask.size()
 
+        if attention_mask is not None:
             attention_mask = attention_mask.reshape(batch_size, 1, query_length, key_length)
             attention_mask = attention_mask.to(attention_logits.dtype) * finfo(attention_logits.dtype).min
             attention_logits: MultiHeadedAttention = attention_logits + attention_mask
+
+        if self.has_relative_attention_bias:
+            position_bias = self.compute_bias(query_length, key_length)
+            attention_logits: MultiHeadedAttention = attention_logits + position_bias
 
         attention_probs: MultiHeadedAttention = attention_logits.softmax(dim=3)
         attention_probs: MultiHeadedAttention = dropout(attention_probs, p=config.dropout_prob, training=self.training)
@@ -289,6 +294,70 @@ class BaselineAttention(AttentionBase):
         attention_output: SequenceInputEmbeds = self.w_o(attention_values)
 
         return AttentionOutput(attention_output, (key, value))
+
+    @staticmethod
+    def _relative_position_bucket(relative_position, bidirectional=True, num_buckets=32, max_distance=128):
+        """
+        Adapted from Mesh Tensorflow:
+        https://github.com/tensorflow/mesh/blob/0cb87fe07da627bf0b7e60475d59f95ed6b5be3d/mesh_tensorflow/transformer/transformer_layers.py#L593
+
+        Translate relative position to a bucket number for relative attention. The relative position is defined as
+        memory_position - query_position, i.e. the distance in tokens from the attending position to the attended-to
+        position. If bidirectional=False, then positive relative positions are invalid. We use smaller buckets for
+        small absolute relative_position and larger buckets for larger absolute relative_positions. All relative
+        positions >=max_distance map to the same bucket. All relative positions <=-max_distance map to the same bucket.
+        This should allow for more graceful generalization to longer sequences than the model has been trained on
+
+        Args:
+            relative_position: an int32 Tensor
+            bidirectional: a boolean - whether the attention is bidirectional
+            num_buckets: an integer
+            max_distance: an integer
+
+        Returns:
+            a Tensor with the same shape as relative_position, containing int32 values in the range [0, num_buckets)
+        """
+        relative_buckets = 0
+        if bidirectional:
+            num_buckets //= 2
+            relative_buckets += (relative_position > 0).to(torch.long) * num_buckets
+            relative_position = torch.abs(relative_position)
+        else:
+            relative_position = -torch.min(relative_position, torch.zeros_like(relative_position))
+        # now relative_position is in the range [0, inf)
+
+        # half of the buckets are for exact increments in positions
+        max_exact = num_buckets // 2
+        is_small = relative_position < max_exact
+
+        # The other half of the buckets are for logarithmically bigger bins in positions up to max_distance
+        relative_position_if_large = max_exact + (
+            torch.log(relative_position.float() / max_exact)
+            / math.log(max_distance / max_exact)
+            * (num_buckets - max_exact)
+        ).to(torch.long)
+        relative_position_if_large = torch.min(
+            relative_position_if_large, torch.full_like(relative_position_if_large, num_buckets - 1)
+        )
+
+        relative_buckets += torch.where(is_small, relative_position, relative_position_if_large)
+        return relative_buckets
+
+    def compute_bias(self, query_length, key_length):
+        """Compute binned relative position bias"""
+        device = self.relative_attention_bias.weight.device
+        context_position = torch.arange(query_length, dtype=torch.long, device=device)[:, None]
+        memory_position = torch.arange(key_length, dtype=torch.long, device=device)[None, :]
+        relative_position = memory_position - context_position  # shape (query_length, key_length)
+        relative_position_bucket = self._relative_position_bucket(
+            relative_position,  # shape (query_length, key_length)
+            bidirectional=(not self.is_decoder),
+            num_buckets=self.relative_attention_num_buckets,
+            max_distance=self.relative_attention_max_distance,
+        )
+        values = self.relative_attention_bias(relative_position_bucket)  # shape (query_length, key_length, num_heads)
+        values = values.permute([2, 0, 1]).unsqueeze(0)  # shape (1, num_heads, query_length, key_length)
+        return values
 
 
 class BaselineFeedforward(TransformerComponentBase):
