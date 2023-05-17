47b242a2b48f70665f47fab6a8689287dd644b2f
hlillemark <hlillemark@gmail.com>
2023-05-17 14:49:34 -0700

    Update max steps

--- march/experiments/tests.py
+++ march/experiments/tests.py
@@ -6,6 +6,8 @@ from march.models.no_ff import NoFFTransformer
 
 from march.experiments.baseline import BaselineExperiment
 
+from transformers import Seq2SeqTrainingArguments
+
 
 class MoreHeadsLessQKVDimExperiment(BaselineExperiment):
     def get_model(self) -> TransformerBase:
@@ -173,12 +175,22 @@ class TPWeightsReduceLayersExperiment(BaselineExperiment):
 
 
 class NoFFExperiment(BaselineExperiment):
+    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+        default_training_arguments = self.load_default_training_arguments()
+        default_training_arguments["max_steps"] = 1000
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+
     def get_model(self) -> TransformerBase:
         config = TransformerConfig()
         return NoFFTransformer(config)
 
 
 class NoFFParamMatchExperiment(BaselineExperiment):
+    def get_training_arguments(self) -> Seq2SeqTrainingArguments:
+        default_training_arguments = self.load_default_training_arguments()
+        default_training_arguments["max_steps"] = 1000
+        return Seq2SeqTrainingArguments(self.output_dir, **default_training_arguments)
+
     def get_model(self) -> TransformerBase:
         config = TransformerConfig(dim_model=1088)
         return NoFFTransformer(config)
