c880a9d8ae7d1cdf06c5c5909289cddc42f9134f
bri25yu <bri25yu@gmail.com>
2023-05-16 22:22:49 -0700

    add create c4 dataset

--- march/datasets/c4.py
+++ march/datasets/c4.py
@@ -1,5 +1,7 @@
 from typing import Dict, List
 
+import march  # Redirect cache
+
 from datasets import DatasetDict, load_dataset
 
 from transformers import AutoTokenizer, PreTrainedTokenizerFast
@@ -90,3 +92,58 @@ def create_c4_10m(test: bool=False) -> None:
 
 def load_c4() -> DatasetDict:
     return load_dataset(f"hlillemark/{C4_T5_10M_NAME}")
+
+
+def tokenize_c4() -> None:
+    tokenizer: PreTrainedTokenizerFast = load_c4_tokenizer()
+    def tokenize_fn(examples: Dict[str, List[str]]) -> Dict[str, List[int]]:
+        return {"input_ids": tokenizer(examples["text"]).input_ids}
+
+    dataset_dict = DatasetDict({
+        "train": load_dataset("c4", "en", split="train"),
+        "validation": load_dataset("c4", "en", split=f"validation[:{3 * NUM_VAL_EXAMPLES}]"),
+    })
+    tokenized_dataset_dict = dataset_dict.map(tokenize_fn, batched=True, remove_columns=dataset_dict["train"].column_names, desc="Tokenizing", num_proc=16)
+    tokenized_dataset_dict.push_to_hub("c4_t5")
+
+
+def pack_tokenized_c4() -> None:
+    def pack_fn(examples: Dict[str, List[int]]) -> Dict[str, List[int]]:
+        outputs = {"input_ids": []}
+        current = []
+        for input_ids in examples["input_ids"]:
+            current.extend(input_ids)
+            while len(current) > MAX_LENGTH:
+                outputs["input_ids"].append(current[:MAX_LENGTH])
+                current = current[MAX_LENGTH:]
+        return outputs
+
+    tokenized_dataset_dict = load_dataset("hlillemark/c4_t5")
+    packed_dataset_dict = tokenized_dataset_dict.map(pack_fn, batched=True, desc="Packing", num_proc=16)
+    packed_dataset_dict = DatasetDict({
+        "train": packed_dataset_dict["train"],
+        "validation": packed_dataset_dict["validation"].select(range(NUM_VAL_EXAMPLES)),
+    })
+    packed_dataset_dict.push_to_hub("c4_t5_packed")
+
+
+def span_corrupt_packed_c4() -> None:
+    tokenizer: PreTrainedTokenizerFast = load_c4_tokenizer()
+    sentinel_start_id = tokenizer.convert_tokens_to_ids(EXTRA_ID_TOKENS[-1])
+    def apply_span_corruption(examples: Dict[str, List[int]]) -> Dict[str, List[int]]:
+        outputs = {"input_ids": [], "labels": []}
+        for input_ids in examples["input_ids"]:
+            corrupted_input_ids, label_ids = create_span_corrupt_inputs(
+                input_ids, MASK_PROB, AVERAGE_SPAN_LENGTH, sentinel_start_id
+            )
+            outputs["input_ids"].append(corrupted_input_ids)
+            outputs["labels"].append(label_ids)
+
+        return outputs
+
+    packed_dataset_dict = load_dataset("hlillemark/c4_t5_packed")
+    span_corrupted_dataset_dict = packed_dataset_dict.map(apply_span_corruption, batched=True, num_proc=16, desc="Applying span corruption")
+    span_corrupted_dataset_dict.push_to_hub("c4_t5_pretrain")
+
+    # Load span corrupted dataset
+    span_corrupted_dataset_dict = load_dataset("hlillemark/c4_t5_pretrain")
---
+++ scripts/create_c4.py
@@ -0,0 +1,6 @@
+from march.datasets.c4 import tokenize_c4, pack_tokenized_c4, span_corrupt_packed_c4
+
+
+tokenize_c4()
+pack_tokenized_c4()
+span_corrupt_packed_c4()
