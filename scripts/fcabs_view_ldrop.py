79f8184ca4eb302255ef36c8bf4af15703f8f6c3
bri25yu <bri25yu@gmail.com>
2023-05-26 14:24:53 -0700

    using make_ndarray

--- scripts/fcabs_view_ldrop.py
+++ scripts/fcabs_view_ldrop.py
@@ -2,6 +2,7 @@ from typing import List, Tuple
 
 from numpy import array
 
+from tensorflow import make_ndarray
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
@@ -9,10 +10,10 @@ def get_dropped_ids(path: str) -> Tuple[List[float], List[float]]:
     event_accumulator = EventAccumulator(path)
     event_accumulator.Reload()
 
-    texts = event_accumulator.Tensors("eval/global_dropped_ids_by_layer")
+    texts = event_accumulator.Tensors("eval/global_dropped_ids_by_layer/text_summary")
 
     steps = array([s.step for s in texts])
-    values = array([s.value for s in texts])
+    values = array([make_ndarray(s.tensor_proto) for s in texts])
 
     return steps, values
 
