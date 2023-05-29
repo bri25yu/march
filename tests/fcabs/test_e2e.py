7e7f25dca470cfa50dcaff6bffa0512cd28ba3ec
bri25yu <bri25yu@gmail.com>
2023-05-28 18:59:53 -0700

    fixing test e2e with unittest

--- tests/fcabs/test_e2e.py
+++ tests/fcabs/test_e2e.py
@@ -1,6 +1,10 @@
-from unittest import TestCase
+import march  # Redirect cache. This is necessary because this file might be run as a standalone
+
+from sys import argv
 
 from os.path import exists
+
+from unittest import TestCase, main as unittest_main
 
 from tests.fcabs.experiment_mixins import TestFCABSExperiment
 
@@ -9,3 +13,8 @@ class TestFCABSEndToEnd(TestCase):
     def test_end_to_end(self) -> None:
         exp = TestFCABSExperiment()
         if not exists(exp.output_dir): exp.train()
+
+
+if __name__ == "__main__":
+    unittest_args = argv[:1]
+    unittest_main(argv=unittest_args)
