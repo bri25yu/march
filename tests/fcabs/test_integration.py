b4470fb2536341ab7f5ce795c603dc9933744812
bri25yu <bri25yu@gmail.com>
2023-05-28 20:03:40 -0700

    setting cuda visible devices for cpu fcabs exp

--- tests/fcabs/test_integration.py
+++ tests/fcabs/test_integration.py
@@ -1,4 +1,13 @@
+from os import environ
+environ["CUDA_VISIBLE_DEVICES"] = ""
+
+import march  # Redirect cache. This is necessary because this file might be run as a standalone
+
+from sys import argv
+
 from os.path import exists
+
+from unittest import TestCase, main as unittest_main
 
 from unittest import TestCase
 
@@ -9,3 +18,8 @@ class TestFCABSIntegration(TestCase):
     def test_cpu(self) -> None:
         exp = TestFCABSCPUExperiment()
         if not exists(exp.output_dir): exp.train()
+
+
+if __name__ == "__main__":
+    unittest_args = argv[:1]
+    unittest_main(argv=unittest_args)
