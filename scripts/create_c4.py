08e6e5956a10e9c99246959f1e0f2cd0f5053ab9
bri25yu <bri25yu@gmail.com>
2023-05-30 22:21:00 -0700

    removing type kwarg from store true argument

--- scripts/create_c4.py
+++ scripts/create_c4.py
@@ -5,7 +5,7 @@ from march.datasets.c4 import create_span_corrupted_c4
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--use_tiny", action="store_true", type=bool, default=True)
+    parser.add_argument("--use_tiny", action="store_true", default=True)
     args = parser.parse_args()
 
     create_span_corrupted_c4(use_tiny=args.use_tiny)
