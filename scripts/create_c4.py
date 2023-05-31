3597f816df2e6425aee62414a7f8d0c06c1e195d
bri25yu <bri25yu@gmail.com>
2023-05-31 11:16:36 -0700

    inverting create c4 script flag

--- scripts/create_c4.py
+++ scripts/create_c4.py
@@ -5,7 +5,7 @@ from march.datasets.c4 import create_span_corrupted_c4
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--use_tiny", action="store_true", default=True)
+    parser.add_argument("--create_full", action="store_true")
     args = parser.parse_args()
 
-    create_span_corrupted_c4(use_tiny=args.use_tiny)
+    create_span_corrupted_c4(use_tiny=not args.create_full)
