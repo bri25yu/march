be0e3129d2277f5f698b34b77d793dd9f266423a
bri25yu <bri25yu@gmail.com>
2023-06-06 13:44:58 -0700

    using mem efficient attention

--- march/models/baseline_v2.py
+++ march/models/baseline_v2.py
@@ -105,9 +105,9 @@ class BaselineV2Attention(TransformerComponentBase):
 
         query, key = self.rotary_emb(query, key)
 
-        is_gpu = query.is_cuda  # Use flash attention if on gpu, otherwise use math
+        is_gpu = query.is_cuda  # Use mem efficient attention if on gpu, otherwise use math
         with sdp_kernel(
-            enable_flash=is_gpu, enable_math=not is_gpu, enable_mem_efficient=False
+            enable_flash=False, enable_math=not is_gpu, enable_mem_efficient=is_gpu
         ):
             attention_values: NHLDkv = attention(query, key, value, attention_mask)
 
